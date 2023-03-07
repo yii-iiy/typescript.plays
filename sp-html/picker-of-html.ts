c2673ffd64b717fb77c1c1bc344e6875566d32f8
yii iiy <126449437+yii-iiy@users.noreply.github.com>
2023-03-07 11:51:49 +0800

    Update picker-of-html.ts

--- sp-html/picker-of-html.ts
+++ sp-html/picker-of-html.ts
@@ -25,36 +25,77 @@ pickhtml({path: "docs/current/develop/event-listener.html", host: "trino.io", pi
 
 ////////////////////////////////////////////
 
+// 莫得重试。
 
-const pickwebhtml =
-    (webmessage: {sitehost: string, mainpath: string, picker: (d: Document, p: string) => string }) => 
+// const promise_unwarpper: <T>(p: PromiseLike<T> | Promise<T> | T) => Awaited<PromiseLike<T> | Promise<T> | T> = 
+//     async <T,> (p: PromiseLike<T> | Promise<T> | T) => await p ;
+
+// const promise_unwarpper_ugly: <T>(p: PromiseLike<T> | Promise<T> | T) => Awaited<PromiseLike<T> | Promise<T> | T> = 
+//     <T,> (p: PromiseLike<T> | Promise<T> | T): Awaited<PromiseLike<T> | Promise<T> | T> =>
+//     { 
+//         let ret: T ; p.then(x=> ret=x); return ret;} ;
+
+const unwrap_promise =
+    <T,> (p: T | Promise<T> | PromiseLike<T>)
+    : Awaited<T | Promise<T> | PromiseLike<T>> =>
+        Promise.resolve(p).then(res => res) as Awaited<T | Promise<T> | PromiseLike<T>> ;
+
+
+
+const pick_webhtml =
+    (webmessage: {sitehost: string, mainpath: string, picker: (d: Document, p: string) => string })
+    : Promise<string> => 
     {
-        const pickperhtml =
-            (msg: {path: string, host: string, picker: (d: Document, p: string) => string}) =>
-                fetch(`https://${msg.host}/${msg.path}`)
-                    .then(r => r.text())
+        const pick_perhtml =
+            (msg: {per_path: string, main_hostpath: string, picker: (d: Document, p: string) => string})
+            : Promise<string> =>
+                fetch(`https://${msg.main_hostpath}/${msg.per_path}`)
+                    .then
+                    ( response => response.text()
+                    , reject => `<!-- [ERROR] :pick_perhtml: fetch 'https://${msg.main_hostpath}/${msg.per_path}' got reject: ${reject} -->`
+                    )
                     .then(t => (new DOMParser()).parseFromString(t,"text/html"))
-                    .then(document => msg.picker(document,msg.path) )
-
-        fetch(`https://${webmessage.sitehost}/${webmessage.mainpath}`)
-            .then(r => r.text())
+                    .then(document => msg.picker(document,msg.per_path) )
+                    .catch(err => `<!-- [FAILED] :pick_perhtml: job on 'https://${msg.main_hostpath}/${msg.per_path}' failed: ${err} -->`)
+        
+        return fetch(`https://${webmessage.sitehost}/${webmessage.mainpath}`)
+            .then
+            ( response => response.text()
+            , reject => `<!-- [ERROR] :pick_webhtml: fetch 'https://${webmessage.sitehost}/${webmessage.mainpath}' got reject: ${reject} -->` 
+            )
             .then(t => (new DOMParser()).parseFromString(t,"text/html"))
             .then
-            (htmldom => 
+            ( htmldom => 
                 Array.from( htmldom.getElementsByClassName("reference internal") )
                     .map(elem => `${webmessage.mainpath}/${elem.getAttribute("href") ?? ""}`)
-                    .map(pathper => {return {path: pathper, host: webmessage.sitehost, picker: webmessage.picker}})
-                    .map(msgper => pickperhtml(msgper))
-                    .reduce((x,y)=>x+y)
+                    .map(pathper => {return {per_path: pathper, main_hostpath: `${webmessage.sitehost}`, picker: webmessage.picker}})
+                    .map(msgper => pick_perhtml(msgper))
             )
-            .then( (tg: Array<string>) => tg.forEach(t=>console.log(t) ) )
-    }
+            .then(htmparts_promising => Promise.all(htmparts_promising) )
+            .then(htmparts => htmparts.reduce((x,y) => x+y))
+            .then(partsfull => ["<div path=docs/current/>",partsfull,"</div>"].reduce((x,y) => x+y))
+            .catch(err => `<!-- [FAILED] :pick_webhtml: job on 'https://${webmessage.sitehost}/${webmessage.mainpath}' failed: ${err} -->`)
+    } ;
 
-// 这个还没写好，类型对不上。
-// 我需要明白 TND await 到底是 TND Promise 的怎么着的语法糖。
-// 不知为啥就是查不到，任何资料，查不到。
+const trino_picker = 
+    (document: Document, path: string)
+    : string => 
+        [ `<div path=${path} >`
+        , document.getElementsByClassName("md-container")[0].outerHTML
+        , document.getElementsByClassName("md-footer")[0].outerHTML
+        , "</div>" ] .reduce ( (x,y) => x+y ) ;
 
-// 我的需要很简单。就是把东西取出来， fetch() 取不到就给我重试。
-// 在 shell 里就是很简单的 while ! (commands paras ...); do echo retried $((t++)); done
-// 或者玩儿的花一点儿就是 retrier () { "$@" || exec bash -c "(declare -f -- retrier) && retrier $(for p in "$@"; do printf %s "'$p' "; done)" ; } 这样子
-// Promise 作为一个可能失败因而取不到目标值的操作没有自带的重试实现就真的很离谱。。。
+const trino_msg = {sitehost: "trino.io", mainpath: "docs/current/", picker: trino_picker} ;
+
+const picking = pick_webhtml(trino_msg) ;
+
+picking.then(res => console.log(res))
+
+
+///////////////////////////
+
+
+
+
+
+
